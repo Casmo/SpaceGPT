9eef6ad572d9810eb9b2ec5ca56aaf0632f6bb96
Mathieu <mathieu@fellicht.nl>
2022-12-29 15:12:04 +0100

    Make the player shoot when the mouse is down instead of on click.

--- js/player.js
+++ js/player.js
@@ -1,21 +1,50 @@
 // Create the player object
 var player = {
-    // Set the initial position of the player to the center of the canvas
-    x: 500,
-    y: 500,
-  
-    // Set the size of the player
-    size: 50,
-  
-    // Define the render function
-    render: function() {
-      // Get the context of the canvas
-      var ctx = canvas.getContext("2d");
-  
-      // Set the fill color of the player
-      ctx.fillStyle = "red";
-  
-      // Draw a filled rectangle at the current position of the player
-      ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+  // Set the initial position of the player to the center of the canvas
+  x: 500,
+  y: 500,
+
+  canShoot: true,
+  shootInterval: 750,
+
+  // Set the size of the player
+  size: 50,
+
+  // Define the update function
+  update: function () {
+    // Update the position of the player
+    this.x = mouseX;
+    this.y = mouseY;
+
+    // Check if the mouse is down and the player is allowed to shoot
+    if (mouseDown && this.canShoot) {
+      // Add a new bullet
+      addBullet(this.x, this.y);
+
+      // Set the canShoot flag to false
+      this.canShoot = false;
+
+      // Set a timeout to allow the player to shoot again after a certain amount of time
+      setTimeout(function () {
+        player.canShoot = true;
+      }, player.shootInterval);
     }
-  };
+  },
+
+  // Define the render function
+  render: function () {
+    // Get the context of the canvas
+    var ctx = canvas.getContext("2d");
+
+    // Set the fill color of the player
+    ctx.fillStyle = "red";
+
+    // Draw a filled rectangle at the current position of the player
+    ctx.fillRect(
+      this.x - this.size / 2,
+      this.y - this.size / 2,
+      this.size,
+      this.size
+    );
+  },
+};
